8720e3c9fc132c41ba96e30fccc56381bd85dcf3
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-20 00:55:57 +0500

    Create Our Mission Section to the page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,15 @@
 import AboutCarousel from "@/components/About/Carousel";
 import TrustSection from "@/components/About/TrustSection";
+import OurMission from "@/components/About/OurMission";
+
 
 export default function AllIndustries() {
   return (
     <main className="min-h-screen bg-backgroundLight flex flex-col justify-center items-center mt-0 md:mt-15">
         <AboutCarousel/>
         <TrustSection/>
+        <OurMission/>
+
     </main>
   );
 }
