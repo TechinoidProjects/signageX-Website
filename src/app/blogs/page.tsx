e3ac8572f6b2397366629a5c1298ecf63afb7172
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-21 17:40:26 +0500

    Component: Create Hero Section component and include it in the Blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,8 @@
+import Hero from "@/components/Blog/Hero";
+export default function AllBlogs() {
+  return (
+    <main className="min-h-screen bg-backgroundLight flex flex-col justify-start items-center  mt-0 md:mt-15">
+        <Hero/>
+    </main>
+  );
+}
