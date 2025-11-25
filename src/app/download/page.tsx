a158dd89f89ab6d41a36790945b6d313c12c5f78
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-25 16:24:28 +0500

    Complete the page modify the download card as well

---
+++ src/app/download/page.tsx
@@ -0,0 +1,11 @@
+import MainSection from "@/components/download/MainSection";
+import CTA from "@/components/common/CTA";
+
+export default function AllBlogs() {
+  return (
+    <main className="min-h-screen bg-backgroundLight flex flex-col justify-start items-center  mt-0 md:mt-15">
+        <MainSection/>
+        <CTA/>
+    </main>
+  );
+}
