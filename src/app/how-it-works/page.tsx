3ba2ca6466d4d596c3e55ac4df20902d9ec74051
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-26 16:45:35 +0500

    Create and add Hero Section for the Page

---
+++ src/app/how-it-works/page.tsx
@@ -0,0 +1,11 @@
+
+import Hero from "@/components/HowItWorks/Hero";
+import CTA from "@/components/common/CTA/index"
+export default function AllIndustries() {
+  return (
+    <main className="min-h-screen bg-backgroundLight flex flex-col justify-center items-center mt-15">
+      <Hero />
+      <CTA/>
+    </main>
+  );
+}
