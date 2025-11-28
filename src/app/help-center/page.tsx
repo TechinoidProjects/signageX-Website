a6b36aecf8e1e979d869cba9ddad51454956aaa4
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-28 20:06:24 +0500

    feat: Create Responsive FAQ Component and add All FAQs in the section

--- src/app/help-center/page.tsx
+++ src/app/help-center/page.tsx
@@ -2,6 +2,7 @@
 import Hero from "@/components/HelpCenter/Hero";
 import HelpArticles from "@/components/HelpCenter/HelpArticles";
 import FreeTrialSection from "@/components/common/FreeTrialSection";
+import AllFAQs from "@/components/HelpCenter/AllFAQs";
 export default function HowItWorks() {
   return (
     <main className="min-h-screen bg-white flex flex-col justify-start items-center mt-15">
@@ -12,6 +13,7 @@ export default function HowItWorks() {
         subheading="No hidden fees"
         buttonText="Start Free Trial – No Card Needed"
       />
+      <AllFAQs />
       
     </main>
   );
