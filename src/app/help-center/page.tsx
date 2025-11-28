21fa97a1c241733375fdcb4c25cdfca6cfcb7a1d
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-28 17:39:31 +0500

    Update download card and add to Help Articles section and add this section into the page

--- src/app/help-center/page.tsx
+++ src/app/help-center/page.tsx
@@ -1,9 +1,11 @@
 
 import Hero from "@/components/HelpCenter/Hero";
+import HelpArticles from "@/components/HelpCenter/HelpArticles";
 export default function HowItWorks() {
   return (
     <main className="min-h-screen bg-white flex flex-col justify-start items-center mt-15">
       <Hero />
+      <HelpArticles />
       
     </main>
   );
