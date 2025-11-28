3b873cb182db176e480496705d7fd7c82a4e853a
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-28 19:36:49 +0500

    feat: add Free Trial section to page
    - Created new Free Trial section component
    - Integrated it into the main page layout
    - Update the Tailwind CSS and global CSS according to the need

--- src/app/help-center/page.tsx
+++ src/app/help-center/page.tsx
@@ -1,11 +1,17 @@
 
 import Hero from "@/components/HelpCenter/Hero";
 import HelpArticles from "@/components/HelpCenter/HelpArticles";
+import FreeTrialSection from "@/components/common/FreeTrialSection";
 export default function HowItWorks() {
   return (
     <main className="min-h-screen bg-white flex flex-col justify-start items-center mt-15">
       <Hero />
       <HelpArticles />
+      <FreeTrialSection
+        heading="Try SignageX For Free"
+        subheading="No hidden fees"
+        buttonText="Start Free Trial – No Card Needed"
+      />
       
     </main>
   );
---
+++ src/components/common/FreeTrialSection.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { Heading } from "./MainHeading"; 
+import { Dice2, Dice1 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+interface FreeTrialSectionProps {
+  heading: string;
+  subheading: string;
+  buttonText: string;
+}
+
+const FreeTrialSection: React.FC<FreeTrialSectionProps> = ({
+  heading,
+  subheading,
+  buttonText,
+}) => {
+  return (
+    <section className=" relative w-full overflow-hidden py-20 md:py-28 bg-gradient-deep flex flex-col items-center text-center">
+      
+      <Dice2
+        className="absolute z-1 top-[-70] left-[-80] text-purple-400/20 w-40 h-40 md:w-70 md:h-70 rotate-90"
+        strokeWidth={1.5}
+      />
+      <div className="bg-deep-purple-500 rotate-180 shadow-sm h-25 w-25 rounded-2xl absolute top-30 left-30"></div>
+
+      <Dice1
+        className="absolute z-1 top-60 right-[-150] text-purple-400/20 w-40 h-40 md:w-70 md:h-70 rotate-90"
+        strokeWidth={1.5}
+      />
+      <div className="bg-deep-purple-500 rotate-180 shadow-sm h-25 w-25 rounded-2xl absolute top-60 right-15"></div>
+
+      {/* CONTENT */}
+      <Heading
+        title={heading}
+        subtitle={subheading}
+        titleColorClass="text-white"
+        subtitleColorClass="text-white/80"
+        size="lg"
+      /> 
+      <Button
+        color="white"
+        size="lg"
+        className="mt-5 mx-auto"
+        href="#"
+      >
+        {buttonText}
+      </Button>
+    </section>
+  );
+};
+
+export default FreeTrialSection;
