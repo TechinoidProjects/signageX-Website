b1d06357eb13aeb474f565fdf91a9b1d77d16178
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-11-28 20:09:15 +0500

    Fix: Resolve the Responsiveness Issues

--- src/components/common/FreeTrialSection.tsx
+++ src/components/common/FreeTrialSection.tsx
@@ -22,10 +22,10 @@ const FreeTrialSection: React.FC<FreeTrialSectionProps> = ({
         className="absolute z-1 top-[-70] left-[-80] text-purple-400/20 w-40 h-40 md:w-70 md:h-70 rotate-90"
         strokeWidth={1.5}
       />
-      <div className="bg-deep-purple-500 rotate-180 shadow-sm h-25 w-25 rounded-2xl absolute top-30 left-30"></div>
+      <div className="md:flex bg-deep-purple-500 rotate-180 shadow-sm h-15 w-15 md:h-25 md:w-25 md:rounded-2xl rounded-xl absolute md:top-30 md:left-30 top-10 left-5"></div>
 
       <Dice1
-        className="absolute z-1 top-60 right-[-150] text-purple-400/20 w-40 h-40 md:w-70 md:h-70 rotate-90"
+        className="absolute z-1 top-60 right-[-50] md:right-[-150] text-purple-400/20 w-40 h-40 md:w-70 md:h-70 rotate-90"
         strokeWidth={1.5}
       />
       <div className="bg-deep-purple-500 rotate-180 shadow-sm h-25 w-25 rounded-2xl absolute top-60 right-15"></div>
