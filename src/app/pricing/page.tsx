056917415b3441c7dc85763ff34dca411a909c8c
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-12-01 17:29:18 +0500

    feat: modify the Signage Pricing card and create the hero section for the page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,17 @@
+
+import SignagePricing from "@/components/HomePage/SignagePricing";
+import CTA from "@/components/common/CTA";
+export default function HowItWorks() {
+  return (
+    <main className="min-h-screen bg-white flex flex-col justify-start items-center mt-15">
+        <SignagePricing
+            badgeText="Pricing"
+            heading="Simple Plans, Smarter Signage"
+            subheading="Choose a flexible pricing plan that fits your business. Scale effortlessly as you grow with AI-powered digital signage tools."
+        />
+      
+      <CTA />
+      
+    </main>
+  );
+}
