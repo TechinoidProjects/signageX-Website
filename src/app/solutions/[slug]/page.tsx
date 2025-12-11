d5d371561777c43b3b0b2ec6164d5bd932309c1a
Muhammad Qayyum Anjum <muhammadqayum014@gmail.com>
2025-12-11 15:09:48 +0500

    feat: Create component for features and add it in to the page

--- src/app/solutions/[slug]/page.tsx
+++ src/app/solutions/[slug]/page.tsx
@@ -3,6 +3,7 @@ import CTA from "@/components/common/CTA";
 import ChallengeSection from "@/components/Solutions/ChallengeSection";
 import SolutionHighlightSection from "@/components/Solutions/SolutionHighlightSection";
 import { FeatureTabs } from "@/components/HomePage/FeaturedTabs";
+import FeaturedApps from "@/components/Industries/FeaturedApps";
 import { notFound } from "next/navigation";
 import { solutions } from '@/lib/solutionsData';
 
@@ -57,7 +58,18 @@ export default async function SolutionsPage({ params }: SolutionPageProps) {
           subheading={solution.featureTabs.subheading}
         />
       )}
-      
+      <FeaturedApps
+        badgeTitle="Ready-to-Use"
+        headingTitle="Free Designs & Templates For You"
+        headingSubtitle="Prebuilt layouts crafted to boost menu appeal instantly."
+        headingTitleColor="text-white"
+        headingSubtitleColor="text-white/90"
+        cards={solution.featuredApps?.cards || []}
+        interval={7000}
+        buttonText="Explore All Retail Widgets"
+        buttonHref="/widgets/retail"
+        shownButton={false}
+      />
       <CTA />
     </main>
   );
--- src/components/Industries/FeaturedApps.tsx
+++ src/components/Industries/FeaturedApps.tsx
@@ -1,8 +1,11 @@
 import { Heading } from "@/components/common/MainHeading";
 import AnimatedCards from "@/components/HomePage/WhyChooseSignageX/AnimatedCards";
+import { Badge } from "../ui/badge-default";
 import { Card } from "@/lib/industriesData";
+import { Dices } from "lucide-react";
 
 interface FeaturedAppsProps {
+  badgeTitle?: string;
   headingTitle?: string;
   headingSubtitle?: string;
   headingTitleColor?: string;
@@ -11,21 +14,29 @@ interface FeaturedAppsProps {
   interval?: number;
   buttonText?: string;
   buttonHref?: string;
+  shownButton?: boolean;
 }
 
 export default function FeaturedApps({
+  badgeTitle="Solutions",
   headingTitle = "Ready-to-Use Apps & Widgets",
   headingSubtitle = "Access a library of pre-built apps and widgets designed for retail, education, healthcare, and more. Easily customize them with AI to create meaningful screen experiences — no effort required.",
   headingTitleColor = "text-white",
   headingSubtitleColor = "text-white",
   cards,
   interval = 5000,
+  shownButton= true,
   buttonText = "View All Widgets/Apps",
   buttonHref = "#",
 }: FeaturedAppsProps) {
   return (
-    <section className="relative w-full bg-deep-purple-500 text-white py-16 overflow-hidden my-6">
+    <section className="relative w-full bg-gradient-primary text-white py-16 overflow-hidden my-6">
       <div className="text-center mb-10 px-4">
+        {badgeTitle && <Badge 
+            icon={Dices}
+            variant="outline"
+            color="white"
+            text={badgeTitle} />}
         <Heading 
           title={headingTitle}
           subtitle={headingSubtitle}
@@ -37,14 +48,16 @@ export default function FeaturedApps({
 
       <AnimatedCards cards={cards} interval={interval} />
 
-      <div className="flex justify-center mt-10">
-        <a
-          href={buttonHref}
-          className="bg-white text-[#1E1045] font-semibold py-3 px-6 rounded-xl hover:bg-white/90 transition"
-        >
-          {buttonText}
-        </a>
-      </div>
+      {shownButton && (
+        <div className="flex justify-center mt-10">
+          <a
+            href={buttonHref}
+            className="bg-white text-[#1E1045] font-semibold py-3 px-6 rounded-xl hover:bg-white/90 transition"
+          >
+            {buttonText}
+          </a>
+        </div>
+      )}
     </section>
   );
 }
